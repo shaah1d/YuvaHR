ca49b42c09a0abda182d48fc1a3ec047c14dd675
Shaahid <shaahid2453@gmail.com>
2024-08-09 03:42:43 -0700

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,6 @@ const multer = require('multer');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
-const allowedOrigins = ['https://yuvahr.com', 'https://www.yuvahr.com'];
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -13,10 +12,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(cors({
-     origin: allowedOrigins,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    
+    origin: 'http://localhost:5173'
 }));
 app.use(express.json());
 
@@ -52,8 +48,32 @@ app.post('/contact', upload.single('file'), (req, res) => {
             },
         ],
     };
-
+    const mailtopeople = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Thank You for Submitting Your Resume to YuvaHr Services',
+        html: `
+        <p>Dear <strong>${name}</strong>,</p>
+        <p>Thank you for choosing YuvaHR Services as your trusted partner in your job search. We appreciate the time you’ve taken to submit your resume and are pleased to confirm that your application has been successfully recorded in our system.</p>
+        <p>Our team will carefully review your qualifications and match them with the opportunities available across our network of esteemed companies. If your profile aligns with the requirements of our clients, we will reach out to you with further information.</p>
+        <p>We understand that finding the right job is important, and we’re committed to assisting you every step of the way. Rest assured, if you are shortlisted for any position, you will hear from us promptly.</p>
+        <p>Thank you once again for your trust in YuvaHR Services. We look forward to the possibility of helping you find your next great opportunity.</p>
+        <p>Best regards,</p>
+        <p><strong>YuvaHR Services</strong></p>
+    `,
+    };
     transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Failed to submit form.');
+        } else {
+            console.log('Email sent: ' + info.response);
+
+            fs.unlinkSync(resumePath);
+            res.status(200).send('Form submitted successfully.');
+        }
+    });
+    transporter.sendMail(mailtopeople, (error, info) => {
         if (error) {
             console.error(error);
             res.status(500).send('Failed to submit form.');
@@ -67,22 +87,10 @@ app.post('/contact', upload.single('file'), (req, res) => {
 });
 
 
-// app.get('/users', async (req, res) => {
-//     try {
-//         const items = await User.find();
-//         res.json(items);
-//     } catch (error) {
-//         res.status(500).send(error);
-//     }
-// });
 
-// app.get('/users/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const item = await User.findById(id);
-//     res.json(item);
-// });
 
 
 app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000');
 });
+
